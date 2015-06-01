fd45ebed5e574276ecc47d8ecfdfe58267b30a0f
Carl Klingberg <carlklingberg@gmail.com>
2015-06-01 16:16:10 +0200

    Replaced livereload with Browsersync

--- gulp/config.template.js
+++ gulp/config.template.js
@@ -4,14 +4,15 @@ var path = require("path");
 
 module.exports = {
     // Path to css, js and img folders
-    root: path.resolve("./"), 
+    root: path.resolve("./"),
     // Path to build folder
-    buildPath: function() { return path.join(this.root, "builds"); }, 
-    // Folders to exclude for watch and jshint
-    jsFoldersToExclude: [ "node_modules" ],
-    // Folders to copy to the build folder
-    jsFoldersToCopy: [], 
+    buildPath: function() { return path.join(this.root, "builds"); },
     // Beep on error
-    beep: true 
+    beep: true,
+    // Folders to exclude for watch and jshint (optionla)
+    jsFoldersToExclude: [ "node_modules" ],
+    // Folders to copy to the build folder (optional)
+    jsFoldersToCopy: [],
+    // Proxy used with browser sync, examples local.dev, localhost:8888 (optional)
+    proxy: ""
 };
-
--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,13 +2,20 @@
 
 var gulp = require("gulp");
 var changed = require("gulp-changed");
-var imagemin = require("gulp-imagemin");
 var path = require("path");
 var config = require("../config");
 
+try {
+    var imagemin = require("gulp-imagemin");
+} catch (er) {
+    imagemin = null;
+}
+
 gulp.task("images", function() {
-	gulp.src(path.join(config.root, "img", "**"))
-		.pipe(changed(path.join(config.buildPath(), "img")))
-		.pipe(imagemin())
-		.pipe(gulp.dest(path.join(config.buildPath(), "img")));
+    if (imagemin) {
+        gulp.src(path.join(config.root, "img", "**"))
+            .pipe(changed(path.join(config.buildPath(), "img")))
+            .pipe(imagemin())
+            .pipe(gulp.dest(path.join(config.buildPath(), "img")));
+    }
 });
