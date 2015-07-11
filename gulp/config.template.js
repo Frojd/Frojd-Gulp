9c4ea42f983085b1384540146b344a0f795c16b6
Carl Klingberg <carlklingberg@gmail.com>
2015-07-11 10:58:17 +0200

    Changed variable names

--- gulp/config.template.js
+++ gulp/config.template.js
@@ -9,10 +9,10 @@ module.exports = {
     buildPath: function() { return path.join(this.root, "builds"); },
     // Beep on error
     beep: true,
-    // Folders to exclude for watch and jshint (optionla)
-    jsFoldersToExclude: [ "node_modules" ],
-    // Folders to copy to the build folder (optional)
-    jsFoldersToCopy: [],
+    // Folders to exclude for watch and jshint (optional)
+    excludedFolders: [ "node_modules" ],
+    // Folders to copy to the build folder (optional). You probably want "fonts"
+    foldersToCopy: [],
     // Proxy used with browser sync, examples local.dev, localhost:8888 (optional)
     proxy: ""
 };
