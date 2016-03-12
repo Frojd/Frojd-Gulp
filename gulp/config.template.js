6a6a4e757f5ceb2c965a49d35fd7753941d09aef
Martin Sandström <martin@marteinn.se>
2016-03-12 18:12:14 +0100

    Added support for copying files to a custom namespace within the build directory

--- gulp/config.template.js
+++ gulp/config.template.js
@@ -3,6 +3,8 @@
 var path = require('path');
 
 module.exports = {
+    // Load specific tasks (empty means load all tasks)
+    tasks: [],
     // Path to css, js and img folders
     root: path.resolve('./'),
     // Path to build folder
@@ -12,5 +14,8 @@ module.exports = {
     // Folders to exclude for watch and jshint (optional)
     excludedFolders: [],
     // Folders to copy to the build folder, you probably want "fonts" (optional)
-    foldersToCopy: []
+    foldersToCopy: [
+        // 'fonts',                 // copy ./fonts to builds/fonts
+        // {'fonts': 'css/fonts'}   // copy ./fonts to builds/css/fonts
+    ]
 };
--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -7,9 +7,20 @@ var config = require('../config');
 var handleErrors = require('../util/handleErrors');
 
 gulp.task('copy', function () {
-    for (var i = 0; i < config.foldersToCopy.length; i++) {
-        gulp.src(path.join(config.root, config.foldersToCopy[i], '**'))
-        .on('error', handleErrors.warning)
-        .pipe(gulp.dest(path.join(config.buildPath(), config.foldersToCopy[i])));
-    }
+    var folders = config.foldersToCopy;
+    folders.forEach(function(folder) {
+        var from = folder;
+        var destination = from;
+
+        if (typeof from === 'object') {
+            var key = Object.keys(folder)[0];
+
+            destination = folder[key];
+            from = key;
+        }
+
+        gulp.src(path.join(config.root, from, '**'))
+            .on('error', handleErrors.warning)
+            .pipe(gulp.dest(path.join(config.buildPath(), destination)));
+    });
 });
