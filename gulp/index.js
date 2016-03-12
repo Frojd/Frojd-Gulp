11698d86509cec5c2036b7264abcd79389cc7573
Martin Sandström <martin@marteinn.se>
2016-03-12 17:01:15 +0100

    Added a nicer error message if config is net yet available

--- gulp/index.js
+++ gulp/index.js
@@ -1,6 +1,15 @@
 'use strict';
 
 var fs = require('fs');
+
+try {
+    var config = require("./config");
+} catch (e) {
+    console.error("Error! Missing config file, have you forgot to "+
+            "copy config.template.js?");
+    process.exit(e.code);
+}
+
 var tasks = fs.readdirSync('./gulp/tasks/');
 
 tasks.forEach(function(task) {
