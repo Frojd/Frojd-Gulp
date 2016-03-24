2c27b707fd01cfc55c74ce780b2ce680a401c4ed
Martin Sandström <martin@marteinn.se>
2016-03-24 18:13:49 +0100

    Fixed broken tests

--- test/test-gulp.js
+++ test/test-gulp.js
@@ -3,33 +3,45 @@ var fs = require('fs');
 
 describe('Assert gulp build works', function() {
     describe('Locate generated files', function () {
-        it('dist directory should exist', function () {
+        it('dist directory should exist', function (done) {
             fs.stat('./test/test-frontend/dist', function(err, stats) {
                 assert.equal(err, null);
+                done();
             });
         });
 
-        it('dist/index.js should exist', function () {
-            fs.stat('./test/test-frontend/js/dist/index.js', function(err, stats) {
+        it('dist/index.js should exist', function (done) {
+            fs.stat('./test/test-frontend/dist/js/index.js', function(err, stats) {
                 assert.equal(err, null);
+                done();
             });
         });
 
-        it('dist/index-min.js should exist', function () {
-            fs.stat('./test/test-frontend/js/dist/index-min.js', function(err, stats) {
+        it('dist/index-min.js should exist', function (done) {
+            fs.stat('./test/test-frontend/dist/js/index-min.js', function(err, stats) {
                 assert.equal(err, null);
+                done();
             });
         });
 
-        it('dist/index.css exist', function () {
-            fs.stat('./test/test-frontend/js/dist/index.css', function(err, stats) {
+        it('dist/index.css exist', function (done) {
+            fs.stat('./test/test-frontend/dist/css/index.css', function(err, stats) {
                 assert.equal(err, null);
+                done();
             });
         });
 
-        it('dist/index-min.css exist', function () {
-            fs.stat('./test/test-frontend/js/dist/index-min.css', function(err, stats) {
+        it('dist/index-min.css exist', function (done) {
+            fs.stat('./test/test-frontend/dist/css/index-min.css', function(err, stats) {
                 assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('dist/fonts/helvetica.txt should exist', function (done) {
+            fs.stat('./test/test-frontend/dist/fonts/helvetica.txt', function(err, stats) {
+                assert.equal(err, null);
+                done();
             });
         });
     });
