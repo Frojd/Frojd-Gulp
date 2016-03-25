72793c4cf06d4a49b568c977d5329c46b918633a
Martin Sandström <martin@marteinn.se>
2016-03-25 08:39:28 +0100

    Renamed project test folder

--- test/test-gulp.js
+++ test/test-gulp.js
@@ -2,44 +2,46 @@ var assert = require('assert');
 var fs = require('fs');
 
 describe('Assert gulp build works', function() {
+    var projectRoot = './test/frontend-project';
+
     describe('Locate generated files', function () {
         it('dist directory should exist', function (done) {
-            fs.stat('./test/test-frontend/dist', function(err, stats) {
+            fs.stat(projectRoot+'/dist', function(err, stats) {
                 assert.equal(err, null);
                 done();
             });
         });
 
         it('dist/index.js should exist', function (done) {
-            fs.stat('./test/test-frontend/dist/js/index.js', function(err, stats) {
+            fs.stat(projectRoot+'/dist/js/index.js', function(err, stats) {
                 assert.equal(err, null);
                 done();
             });
         });
 
         it('dist/index-min.js should exist', function (done) {
-            fs.stat('./test/test-frontend/dist/js/index-min.js', function(err, stats) {
+            fs.stat(projectRoot+'/dist/js/index-min.js', function(err, stats) {
                 assert.equal(err, null);
                 done();
             });
         });
 
         it('dist/index.css exist', function (done) {
-            fs.stat('./test/test-frontend/dist/css/index.css', function(err, stats) {
+            fs.stat(projectRoot+'/dist/css/index.css', function(err, stats) {
                 assert.equal(err, null);
                 done();
             });
         });
 
         it('dist/index-min.css exist', function (done) {
-            fs.stat('./test/test-frontend/dist/css/index-min.css', function(err, stats) {
+            fs.stat(projectRoot+'/dist/css/index-min.css', function(err, stats) {
                 assert.equal(err, null);
                 done();
             });
         });
 
         it('dist/fonts/helvetica.txt should exist', function (done) {
-            fs.stat('./test/test-frontend/dist/fonts/helvetica.txt', function(err, stats) {
+            fs.stat(projectRoot+'/dist/fonts/helvetica.txt', function(err, stats) {
                 assert.equal(err, null);
                 done();
             });
